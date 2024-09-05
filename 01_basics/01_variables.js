d6ea33f849aafdaab2dd3eb5a5271ad99d613327
mrparasraval <mrparasraval@gmail.com>
2024-09-05 18:30:27 +0000

    Container Config Done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12456
+let accountEmail = "kohli18@gmail.com"
+var accountPassword = "12456"
+accountCity = "jaipur"
+
+// accountId = 2 // Note  : Const is Not Changeble Object
+
+accountEmail = "hc@hc.com"
+accountPassword = "2345"
+accountCity = "ladol"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
