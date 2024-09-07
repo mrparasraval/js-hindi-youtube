b265a0a6559f24b1b8f32e3335c29a2e7833c058
mrparasraval <mrparasraval@gmail.com>
2024-09-07 18:25:50 +0000

    Conv & Comparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 
 let boolearnIsLoggedIn = Boolean(isLoggedIn)
-console.log(boolearnIsLoggedIn);
+// console.log(boolearnIsLoggedIn);
 
 // 1 => true 0 => false
 // "" => false
@@ -24,6 +24,66 @@ console.log(boolearnIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String (someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **************** Operation ****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // 2 x 2 x 2 = 8
+// console.log(2/2);
+// console.log(2**4); // 2 x 2 x 2 x 2 = 16
+
+let str1 = "hello"
+let str2 = " neel"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 1 + 2);
+console.log(1 + 1 + "2");
+console.log((3 + 4) * 5 % 3);
+
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// link 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
