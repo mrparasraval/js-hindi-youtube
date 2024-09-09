b834d882812ea77bbe3b6e667935354c662d2815
mrparasraval <mrparasraval@gmail.com>
2024-09-09 17:43:25 +0000

    Datatpyes And Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,37 @@
+// Primitive 
+
+// 7 types ; String, Number, Boolearn, null, Undefied, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const loggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Refrence ( non primitive)
+
+// Array, Object, Functions
+
+const heros = ["Shaktiman","nagraaj","doga"]
+let myObj = {
+    name: "Paras",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof heros);
+
+
