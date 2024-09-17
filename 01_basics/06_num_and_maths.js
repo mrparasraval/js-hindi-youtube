7a21b12a464a87c9dd712c692829f03d71801e3d
mrparasraval <mrparasraval@gmail.com>
2024-09-17 17:21:33 +0000

    Maths

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,48 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN')); // INDIAN MA LAKHVA EN-IN
+
+//+++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4)); // Math.abs -(run) ne + (dhan) ma convert krva
+console.log(Math.round(4.6)); // math.round Apurnak ne Purnak ma feravava
+console.log(Math.ceil(4.2)); // Apurnak Sankhya ne upar(4.2 = 5) ni baaju purnak ma feravava
+console.log(Math.floor(4.9)); // Apurnak Sankhya ne niche (4.9 = 4) ni baaju purnak ma feravava
+console.log(Math.min(4, 3, 6, 8)); // Nani sankhya janava
+console.log(Math.max(4, 3, 6, 8)); // Moti Sankhya Janva
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()* 10) + 1);
+
+const min = 10
+const max = 20
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
